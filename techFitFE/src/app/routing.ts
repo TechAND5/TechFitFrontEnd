d95a2ef508369759c4fa6aa3c83fced003fac37d
Joan <joanpecamp1993@gmail.com>
2017-08-02 23:31:08 +0200

    ok

--- techFitFE/src/app/routing.ts
+++ techFitFE/src/app/routing.ts
@@ -1,8 +1,6 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UsuarioComponent } from './usuario_component/usuario.component';
 import { HabitosComponent } from './habitos_component/habitos.component';
-import { DiaComponent } from './dia_component/dia.component';
 import { DetalleHabitoComponent } from './detalleHabito_component/detalleHabito.component';
 
 const appRoutes: Routes = [
